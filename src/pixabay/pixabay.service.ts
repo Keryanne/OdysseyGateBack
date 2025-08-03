68b983a0f77c6981b0d324cff207e474ff788fc4
Keryanne <47926567+Keryanne@users.noreply.github.com>
2025-08-03 12:43:11 +0200

    Fix pixabay endpoint for images

--- src/pixabay/pixabay.service.ts
+++ src/pixabay/pixabay.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 
@@ -7,9 +7,13 @@ export class PixabayService {
   constructor(private readonly http: HttpService) {}
 
   async searchImages(query: string) {
+  try {
     const url = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${encodeURIComponent(query + ' travel landscape')}&category=travel&image_type=photo&orientation=horizontal&per_page=5`;
-
     const response = await firstValueFrom(this.http.get(url));
     return response.data;
+  } catch (error) {
+    console.error('Pixabay error:', error?.response?.data || error.message);
+    throw new InternalServerErrorException('Erreur lors de la recherche d\'images Pixabay');
   }
+}
 }
